0fb3e04411942aab217080b8fef774d7ed084177
Paul Sinnett <paul.sinnett@gmail.com>
2019-01-30 17:32:37 +0000

    Paranagamage

---
+++ Paranagamage Abeyrathna/Project/Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float force = 100f; //Adding a float value of force to the Sphere to move top to bottom, repeat
+
+
+    void Start()
+    {
+        rb.AddForce(0, -force, 0, ForceMode.Impulse); //Apply the impulse force instantly with a single function call. ... 
+                                                      //This mode is useful for applying forces that happen instantly, such as forces from explosions or collisions. 
+                                                      //In this mode, the unit of the force parameter is applied to the rigidbody as mass*distance/time
+    }
+
+
+
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "UpLimit") //Adding a top object that the Sphere will move to top
+        {
+
+            rb.AddForce(0, -force, 0, ForceMode.Impulse);
+        }
+
+        if (collisionInfo.gameObject.tag == "DownLimit") //Adding a bottom object that the Sphere will move to bottom
+        {
+
+            rb.AddForce(0, force, 0, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) //naming the tag of the object that we want the Player to collect
+        {
+            other.gameObject.SetActive(false); //when collide setting it to de-active
+        }
+
+    }
+
+
+}
---
+++ Paranagamage Abeyrathna/Tutorial 02 - Making the Player Move/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float fowardForce = 900f; //Adding a moving Foward force
+    public float horizontalForce = 500f; //Adding a moving side force
+
+    
+    void FixedUpdate()
+    {
+
+
+
+        rb.velocity = Vector3.zero;
+
+
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //When press "a" moving left
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(-horizontalForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange); //When press "d" moving right
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0 , fowardForce * Time.deltaTime, ForceMode.VelocityChange); //When press "w" moving foward
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -fowardForce * Time.deltaTime, ForceMode.VelocityChange); //When press "s" moving back
+        }
+    }
+}
